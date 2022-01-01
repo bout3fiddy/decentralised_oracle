777c0d9ee69a0986d267b59165c9a5bfaa0ed397
bout3fiddy <11488427+bout3fiddy@users.noreply.github.com>
2022-01-02 00:48:39 +0100

    fixed tests

--- tests/unitary/test_oracle_update.py
+++ tests/unitary/test_oracle_update.py
@@ -1,50 +1,12 @@
 import brownie
-from brownie import chain
 import pytest
 
 
-def test_chainlink_price(deployed_oracle, charlie):
+def test_update_oracle(deployed_oracle, charlie):
 
-    tx = deployed_oracle.get_chainlink_price({"from": charlie})
-    chainlink_price = tx.return_value
-    print(f"chainlink price: {chainlink_price}")
-    assert chainlink_price > 0
+    initial_oracle_price = deployed_oracle.latest_oracle_price()
 
+    tx = deployed_oracle.update_oracle({"from": charlie})
+    oracle_price_post_update = deployed_oracle.latest_oracle_price()
 
-def test_update_oracle(deployed_oracle_with_bounty, charlie, token):
-
-    chain.snapshot()
-    current_block = brownie.web3.eth.get_block_number()
-    print(f"timestamp: {chain.time()}")
-
-    filled_indices = deployed_oracle_with_bounty.filled_indices()
-    print(f"filled indices: {filled_indices}")
-    last_swap_rate = deployed_oracle_with_bounty.last_swap_rate()
-    print(f"last_swap_rate: {last_swap_rate}")
-    average_swap_rate = deployed_oracle_with_bounty.average_swap_rate()
-    print(f"average_swap_rate: {average_swap_rate}")
-
-    tx = deployed_oracle_with_bounty.update_oracle(
-        {"from": charlie}, block_identifier=chain.height
-    )
-    initial_oracle_price = tx.return_value
-
-    filled_indices = deployed_oracle_with_bounty.filled_indices()
-    print(f"filled indices: {filled_indices}")
-    last_swap_rate = deployed_oracle_with_bounty.last_swap_rate()
-    print(f"last_swap_rate: {last_swap_rate}")
-    average_swap_rate = deployed_oracle_with_bounty.average_swap_rate()
-    print(f"average_swap_rate: {average_swap_rate}")
-
-    chain.mine(timedelta=60 * 60)
-
-    tx = deployed_oracle_with_bounty.update_oracle({"from": charlie})
-    oracle_price_post_sleep = tx.return_value
-
-    print("initial oracle price: ", initial_oracle_price)
-    print("oracle price post sleep:", oracle_price_post_sleep)
-
-    assert initial_oracle_price != oracle_price_post_sleep
-    assert token.balanceOf(charlie) > 0
-
-    chain.revert()
+    assert initial_oracle_price != oracle_price_post_update
